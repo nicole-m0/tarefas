3fd15eabc07f68f333f9b0137e0f1a1afed1463e
ifpi <nicole926>
2025-12-11 17:15:59 -0300

    atualizando o CORS

--- tarefas/settings.py
+++ tarefas/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'tarefas.urls'
